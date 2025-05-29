787c3b020ebc76254e75e258d6aa342bdd180592
shynndex <longnguyen352005@gmail.com>
2025-05-29 17:24:44 +0700

    New product show

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,22 +2,31 @@ const Product = require("../../models/product.model");
 
 const productsHelper = require("../../helpers/products");
 
-
 // .index là đặt tên để gọi làm từ bên file khác
 //[GET] /
 module.exports.index = async (req, res) => {
+  //Product featured
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  }).limit(6);
 
-const productsFeatured = await Product.find({
-  featured: "1",
-  deleted:false,
-  status:"active",
-}).limit(6);
+  const newProductFeatured = productsHelper.priceNewProducts(productsFeatured);
+  // End Product featured
 
-const newProduct = productsHelper.priceNewProducts(productsFeatured);
+  //Newest Product
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc" }).limit(6);
 
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+  //End Newest Product
 
-res.render("client/pages/home/index", {
+  res.render("client/pages/home/index", {
     title: "Trang chủ",
-    productsFeatured:newProduct,
+    productsFeatured: newProductFeatured,
+    productsNew: newProductsNew,
   });
 };
