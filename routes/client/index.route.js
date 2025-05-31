e22e53fb2350097ff180dce6bfa76cf7171fc292
shynndex <longnguyen352005@gmail.com>
2025-05-31 16:37:18 +0700

    find product client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,10 +2,14 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
+const searchRoutes = require("./search.route");
+
 // Xuất một hàm nhận app (Express instance) làm tham số.
 module.exports = (app) => {
   app.use(categoryMiddleware.category)
 
   app.use("/", homeRoutes);
   app.use("/products", productRoutes);
+  app.use("/search", searchRoutes);
+
 };
