554f6141f1106d220922331007b4f96df0e118d7
shynndex <longnguyen352005@gmail.com>
2025-06-22 18:09:58 +0700

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -8,14 +8,22 @@ const userSchema = new mongoose.Schema(
     password: String,
     tokenUser: {
       type: String,
-      default:generate.generateRandomString(20),
+      default: generate.generateRandomString(20),
     },
     phone: String,
     avatar: String,
     status: {
-        type:String,
-        default:"active",
+      type: String,
+      default: "active",
     },
+    requestFriends: Array, //Lời mời đã gửi
+    acceptFriends: Array, //Lời mời đã nhận
+    friendList: [ //Danh sách bạn bè
+      {
+        user_id: String,
+        // room_chat_id:String,
+      },
+    ],
     deleted: {
       type: Boolean,
       default: false,
